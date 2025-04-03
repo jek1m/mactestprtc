050055be665da00f7f0b17db64103c18a767b717
jek1m <jieunkim7912@gmail.com>
2025-04-03 13:29:02 +0900

    feat : 바뀌나

--- sww/lib/main.dart
+++ sww/lib/main.dart
@@ -11,7 +11,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Deddddmo',
+      title: 'Flutter Dedddddd바뀌qmo',
       theme: ThemeData(
         // This is the theme of your application.
         //
